4abd6faf240b0b5d3b35aac4717c71d70c870935
Hugo Mozo <hmozo@outlook.com>
2021-02-24 11:22:21 +0100

    Revert "controlled components"
    This reverts commit c8fb52dc6d7c632807e6420244fff5960d641d83.

--- app/App.js
+++ app/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Hello from './Hello'
 import Popular from './components/Popular'
-import Battle from './components/Battle'
 
 class App extends React.Component{
 
     render(){
         return (
             <div className='container'>
-                <Battle />
+                <Popular />
             </div>
         )
     }
