2fdc146892dc889ff75fd95a5ca2c3bbe8734d05
Hugo Mozo <hmozo@outlook.com>
2021-03-01 12:41:25 +0100

    API fetching Github profile & repos

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,3 +1,70 @@
+const id= '6e8798c4459250dc3fe0'
+const sec= 'bfd71225ac6cb0cc55457522a9d6080f8e088148'
+const params= `?client_id=${id}&client_secret=${sec}`
+
+function getErrorMsg(message, username){
+    if(message === 'Not found'){
+        return `${username} doesn't exist`
+    }
+
+    return message
+}
+
+function getProfile(username){
+    return fetch(`https://api.github.com/users/${username}${params}`)
+        .then(res=> res.json())
+        .then(profile=>{
+            if(profile.message){
+                throw new Error(getErrorMsg(profile.message, username))
+            }
+
+            return profile
+        })
+}
+
+function getRepos(username){
+    return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+        .then(res=>res.json())
+        .then(repos=>{
+            if(repos.message){
+                throw new Error(repos.message, username)
+            }
+
+            return repos
+        })
+}
+
+function getStarCount(repos){
+    return repos.reduce((count, {stargazers_count})=>{
+        return count+stargazers_count, 0
+    })
+}
+
+function calculateScore(followers, repos){
+    return (followers*3 + getStarCount(repos))
+}
+
+function getUserData(player){
+    return Promise.all([
+        getProfile(player),
+        getRepos(player),
+    ]).then(([profile, repos])=>({
+        profile,
+        score: calculateScore(profile.followers, repos)
+    }))
+}
+
+function sortPlayers(players){
+    return players.sort((a,b)=> b.score - a.score)
+}
+
+export function battle(players){
+    return Promise.all([
+        getUserData(players[0]),
+        getUserData(players[1])
+    ]).then((results)=> sortPlayers(results))
+}
+
 export function fetchPopularRepos(language){
     const endpoint= window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
