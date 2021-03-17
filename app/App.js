fe03b01e9db0fcf47d1d0eb6ff86a61ac6bf5895
Hugo Mozo <hmozo@outlook.com>
2021-03-17 12:18:57 +0100

    share Theme (Provider/Consumer)

--- app/App.js
+++ app/App.js
@@ -2,14 +2,35 @@ import React from 'react'
 import Hello from './Hello'
 import Battle from './components/Battle'
 import Popular from './components/Popular'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
 class App extends React.Component{
 
+    constructor(props){
+        super(props)
+
+        this.state= {
+            theme: 'light',
+            toggleTheme: ()=>{
+                this.setState(({ theme })=>({
+                    theme:theme==='light'?'dark':'light'
+                }))
+            }
+        }
+    }
+
     render(){
         return (
-            <div className='container'>
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state} >
+                <div className={this.state.theme}>
+                    <div className='container'>
+                        <Nav />
+
+                        <Battle />
+                    </div>
+                </div>
+            </ThemeProvider>
         )
     }
 }
---
+++ app/contexts/theme.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const { Consumer, Provider }= React.createContext()
+
+export const ThemeProvider= Provider
+export const ThemeConsumer= Consumer
+
